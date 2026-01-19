8cadea31f2c892113692d79f80c8b6135e2398bd
moonsakiko <1912690051@qq.com>
2026-01-20 02:18:09 +0800

    测试

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String appName = "PDF书签精灵";
+  static const String appName = "PDF书签工具箱";
   static const bool autoBackup = true; // 是否自动备份原文件
   static const String indentChar = "\t"; // 缩进符，默认 Tab
   static const int maxHistoryFiles = 10; // 历史记录数量 (暂留接口)
